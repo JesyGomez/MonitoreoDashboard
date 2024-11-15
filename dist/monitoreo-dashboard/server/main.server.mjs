8c339013d79541e50a1d326a2374e20d4e8583a6
julianycia.m@gmail.com <julianycia.m@gmail.com>
2024-11-15 20:57:45 -0300

    armando carpeta dist

---
+++ dist/monitoreo-dashboard/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-OK2HAHZ2.mjs";import"./chunk-LIPVUDMR.mjs";import"./chunk-R6EPD4WH.mjs";import"./chunk-RFT6AS4T.mjs";import"./chunk-PCGZHL7I.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
